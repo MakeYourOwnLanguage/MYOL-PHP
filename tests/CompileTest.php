c14e5d7ce33aee393e8959bc5fbffbddf70607ac
Conor  H <conor.howland@outlook.com>
2017-12-20 11:38:23 +0000

    Testing For Compiler

---
+++ tests/CompileTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+require_once 'vendor/autoload.php';
+
+/**
+ * @covers Output
+ */
+final class EmailTest extends TestCase
+{
+
+$argv[1] = "compile.php";
+$argv[2] = "output.php";
+};
